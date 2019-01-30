416f0ca6c9dcd32d008ccf24d36b2f09afcc5230
Robert Kuhlig <16413104+Kuhlig@users.noreply.github.com>
2019-01-30 06:53:41 +0100

    20190130 sql dump v2
    20190130 sql dump v2

---
+++ mysql_dump/20190130_dump_liam2_structure_v2.sql
@@ -0,0 +1,149 @@
+-- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'liam2_User'
+-- 
+-- ---
+CREATE DATABASE `bpmspace_liam2_v2` /*!40100 DEFAULT CHARACTER SET utf8 */;
+use `bpmspace_liam2_v2`;
+
+
+DROP TABLE IF EXISTS `liam2_User`;
+		
+CREATE TABLE `liam2_User` (
+  `liam2_User_id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `liam2_User_firstname` VARCHAR(60) NULL DEFAULT NULL,
+  `liam2_User_lastname` VARCHAR(60) NULL DEFAULT NULL,
+  `liam2_User_password` MEDIUMTEXT NULL DEFAULT NULL,
+  `liam2_User_salt` MEDIUMTEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`liam2_User_id`)
+);
+
+-- ---
+-- Table 'liam2_Role_LIAMUSER'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `liam2_Role_LIAMUSER`;
+		
+CREATE TABLE `liam2_Role_LIAMUSER` (
+  `liam2_user_role_id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `liam2_User_id_fk_134386` BIGINT(20) NOT NULL,
+  `liam2_Role_id_fk_345654` BIGINT(20) NOT NULL,
+  PRIMARY KEY (`liam2_user_role_id`)
+);
+
+-- ---
+-- Table 'liam2_User_LoginAttempt'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `liam2_User_LoginAttempt`;
+		
+CREATE TABLE `liam2_User_LoginAttempt` (
+  `liam2_User_LoginAttempt_id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `liam2_User_id_fk_955788` BIGINT(20) NOT NULL,
+  `liam2_LoginAttempts_id_fk_234561` BIGINT(20) NOT NULL,
+  PRIMARY KEY (`liam2_User_LoginAttempt_id`)
+);
+
+-- ---
+-- Table 'liam2_Role'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `liam2_Role`;
+		
+CREATE TABLE `liam2_Role` (
+  `liam2_Role_id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `liam2_Role_name` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`liam2_Role_id`)
+);
+
+-- ---
+-- Table 'liam2_LoginAttempts'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `liam2_LoginAttempts`;
+		
+CREATE TABLE `liam2_LoginAttempts` (
+  `liam2_LoginAttempts_id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `liam2_LoginAttempts_time` DATETIME NULL DEFAULT NULL,
+  `liam2_LoginAttempts_info` MEDIUMTEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`liam2_LoginAttempts_id`)
+);
+
+-- ---
+-- Table 'liam2_User_email'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `liam2_User_email`;
+		
+CREATE TABLE `liam2_User_email` (
+  `liam2_User_email_id` BIGINT(20) NULL AUTO_INCREMENT DEFAULT NULL,
+  `liam2_User_id_fk_164887` BIGINT(20) NOT NULL,
+  `liam2_email_id_fk_396224` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`liam2_User_email_id`)
+);
+
+-- ---
+-- Table 'liam2_email'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `liam2_email`;
+		
+CREATE TABLE `liam2_email` (
+  `liam2_email_id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `liam2_email_text` VARCHAR(254 ) NULL DEFAULT NULL,
+  PRIMARY KEY (`liam2_email_id`)
+);
+
+-- ---
+-- Foreign Keys 
+-- ---
+
+ALTER TABLE `liam2_Role_LIAMUSER` ADD FOREIGN KEY (liam2_User_id_fk_134386) REFERENCES `liam2_User` (`liam2_User_id`);
+ALTER TABLE `liam2_Role_LIAMUSER` ADD FOREIGN KEY (liam2_Role_id_fk_345654) REFERENCES `liam2_Role` (`liam2_Role_id`);
+ALTER TABLE `liam2_User_LoginAttempt` ADD FOREIGN KEY (liam2_User_id_fk_955788) REFERENCES `liam2_User` (`liam2_User_id`);
+ALTER TABLE `liam2_User_LoginAttempt` ADD FOREIGN KEY (liam2_LoginAttempts_id_fk_234561) REFERENCES `liam2_LoginAttempts` (`liam2_LoginAttempts_id`);
+ALTER TABLE `liam2_User_email` ADD FOREIGN KEY (liam2_User_id_fk_164887) REFERENCES `liam2_User` (`liam2_User_id`);
+ALTER TABLE `liam2_User_email` ADD FOREIGN KEY (liam2_email_id_fk_396224) REFERENCES `liam2_email` (`liam2_email_id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `liam2_User` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `liam2_Role_LIAMUSER` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `liam2_User_LoginAttempt` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `liam2_Role` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `liam2_LoginAttempts` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `liam2_User_email` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `liam2_email` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `liam2_User` (`liam2_User_id`,`liam2_User_firstname`,`liam2_User_lastname`,`liam2_User_email`,`liam2_User_password`,`liam2_User_salt`) VALUES
+-- ('','','','','','');
+-- INSERT INTO `liam2_Role_LIAMUSER` (`liam2_user_role_id`,`liam2_User_id_fk_134386`,`liam2_Role_id_fk_345654`) VALUES
+-- ('','','');
+-- INSERT INTO `liam2_User_LoginAttempt` (`liam2_User_LoginAttempt_id`,`liam2_User_id_fk_955788`,`liam2_LoginAttempts_id_fk_234561`) VALUES
+-- ('','','');
+-- INSERT INTO `liam2_Role` (`liam2_Role_id`,`liam2_Role_name`) VALUES
+-- ('','');
+-- INSERT INTO `liam2_LoginAttempts` (`liam2_LoginAttempts_id`,`liam2_LoginAttempts_time`,`liam2_LoginAttempts_info`) VALUES
+-- ('','','');
+-- INSERT INTO `liam2_User_email` (`liam2_User_email_id`,`liam2_User_id_fk_164887`,`liam2_email_id_fk_396224`) VALUES
+-- ('','','');
+-- INSERT INTO `liam2_email` (`liam2_email_id`,`liam2_email_text`) VALUES
+-- ('','');
