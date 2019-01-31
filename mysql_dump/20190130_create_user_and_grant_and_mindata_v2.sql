77e8096d2409bcfabee0098984003c8e186285f4
Robert Kuhlig <16413104+Kuhlig@users.noreply.github.com>
2019-01-31 11:28:56 +0100

    new minimum data
    new minimum data

--- mysql_dump/20190130_create_user_and_grant_and_mindata_v2.sql
+++ mysql_dump/20190130_create_user_and_grant_and_mindata_v2.sql
@@ -18,15 +18,24 @@ SELECT CONCAT(LPAD(CONV(FLOOR(RAND() * POW(36, 8)), 10, 36), 8, 0),
         LPAD(CONV(FLOOR(RAND() * POW(36, 8)), 10, 36), 8, 0)) INTO @SALTSTRING;
 
 #-- Create Admin
-INSERT INTO `liam_user` (`liam_user_firstname`, `liam_user_lastname`, `liam_user_email`, `liam_user_password`, `liam_user_salt`)
-VALUES ('Administrator', 'Administrator', 'admin@localhost', '', @SALTSTRING);
+INSERT INTO `bpmspace_liam2_v2`.`liam2_User` (`liam2_User_id`, `liam2_User_firstname`, `liam2_User_lastname`, `liam2_User_password`, `liam2_User_salt`, `state_id`) VALUES ('999999999', 'admin', 'admin', '4A711A36ECD079CBC88A1654A9DFD73730C3B131EA1E14BF692A6F97517F445353838750074FC9CF92DE78CE21C65DFB7C8B57653AA7DB1A228077F7B6E088CD', '999999', '8');
 SET @ADMINID = LAST_INSERT_ID();
-UPDATE `liam_user` SET liam_user_password = SHA2(concat('PASSWORT-03-inLASTPASS', liam_user_salt), 512) where liam_user_id = @ADMINID;
+
+INSERT INTO `bpmspace_liam2_v2`.`liam2_email` (`liam2_email_id`, `liam2_email_text`, `state_id`) VALUES ('999', 'something@localhost', '14');
+
+
+SET @EMAILID = LAST_INSERT_ID();
+
+
+INSERT INTO `bpmspace_liam2_v2`.`liam2_User_email` (`liam2_User_email_id`, `liam2_User_id_fk_164887`, `liam2_email_id_fk_396224`, `state_id`) VALUES ('9999', '999999999', '999', '11');
+
 
 #-- Create Role
-INSERT INTO `liam_role` (`liam_role_name`) VALUES ('Administrator');
-SET @ADMINROLEID = LAST_INSERT_ID();
-INSERT INTO `liam_user_role` (`liam_role_id`, `liam_user_id`) VALUES (@ADMINROLEID, @ADMINID);
+
+INSERT INTO `bpmspace_liam2_v2`.`liam2_Role` (`liam2_Role_id`, `liam2_Role_name`, `state_id`) VALUES ('9991', 'Adminstrators', '2');
+
+INSERT INTO `bpmspace_liam2_v2`.`liam2_Role_LIAMUSER` ( `liam2_User_id_fk_134386`, `liam2_Role_id_fk_345654`, `state_id`) VALUES ( '999999999', '9991', '5');
+
 
 #--- User Role
-INSERT INTO `liam_role` (`liam_role_name`) VALUES ('User');
+INSERT INTO `bpmspace_liam2_v2`.`liam2_Role` ( `liam2_Role_name`, `state_id`) VALUES ('Users', '2');
