8217ae74c6268fbea092131051cb3f40e22f56cf
Robert Kuhlig <16413104+Kuhlig@users.noreply.github.com>
2019-05-13 11:55:11 +0200

    Create bpmspace_liam2_v2-config_EXAMPLE_secret.inc.php

---
+++ bpmspace_liam2_v2-config_EXAMPLE_secret.inc.php
@@ -0,0 +1,15 @@
+<?php
+  // Database Login
+  define("DB_USER", "root");
+  define("DB_PASS", "");
+  define("DB_HOST", "localhost");
+  define("DB_NAME", "bpmspace_liam2_v2");
+
+  // API-URL
+  define("API_URL", "");
+  define("API_URL_LIAM", ""); // for Login
+  // AuthKey
+  define("AUTH_KEY", "");
+  // Machine-Token for internal API Calls
+  define("MACHINE_TOKEN", "");
+?>
